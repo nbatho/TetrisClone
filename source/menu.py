34f8b4a594637ebb45dffc34d74b2cd9252c2a6e
NamTG-PTIT <162339079+ntgitis@users.noreply.github.com>
2025-04-25 08:03:09 +0700

    update

--- source/menu.py
+++ source/menu.py
@@ -5,33 +5,28 @@ import os
 SCREEN = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Menu")
 
-BG = pygame.transform.scale(pygame.image.load("F:/Project/[Python] Lap trinh game/TetrisClone/graphics/Background.jpg"), (1280, 720))
-
-
-def get_font(size):
-    return pygame.font.Font("F:/Project/[Python] Lap trinh game/TetrisClone/graphics/font.ttf", size)
-
+BG = pygame.transform.scale(pygame.image.load("E:/Code/Python/Project/TetrisClone/graphics/Background.jpg"), (1280, 720))
 def play_mode_menu():
     selected_index = 0
     buttons = []
 
     SINGLE_BUTTON = Button(
-        image=pygame.image.load("F:/Project/[Python] Lap trinh game/TetrisClone/graphics/Play Rect.png"),
+        image=pygame.image.load("E:/Code/Python/Project/TetrisClone/graphics/Play Rect.png"),
         pos=(640, 220), text_input="SINGLE", font=get_font(40),
         base_color="#d7fcd4", hovering_color="White"
     )
     BOT_BUTTON = Button(
-        image=pygame.image.load("F:/Project/[Python] Lap trinh game/TetrisClone/graphics/BattleAI Rect.png"),
+        image=pygame.image.load("E:/Code/Python/Project/TetrisClone/graphics/BattleAI Rect.png"),
         pos=(640, 340), text_input="vsBOT", font=get_font(40),
         base_color="#d7fcd4", hovering_color="White"
     )
     PLAYER_BUTTON = Button(
-        image=pygame.image.load("F:/Project/[Python] Lap trinh game/TetrisClone/graphics/BattleAI Rect.png"),
+        image=pygame.image.load("E:/Code/Python/Project/TetrisClone/graphics/BattleAI Rect.png"),
         pos=(640, 460), text_input="vsPLAYER", font=get_font(40),
         base_color="#d7fcd4", hovering_color="White"
     )
     BACK_BUTTON = Button(
-        image=pygame.image.load("F:/Project/[Python] Lap trinh game/TetrisClone/graphics/Quit Rect.png"),
+        image=pygame.image.load("E:/Code/Python/Project/TetrisClone/graphics/Quit Rect.png"),
         pos=(640, 580), text_input="BACK", font=get_font(40),
         base_color="#d7fcd4", hovering_color="White"
     )
@@ -72,9 +67,18 @@ def play_mode_menu():
                         return "menu"
         pygame.display.update()
 
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("E:/Code/Python/Project/TetrisClone/graphics/font.ttf", size)
+
+def play():
+    pass
+
 def options():
     while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
         SCREEN.fill("white")
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -82,6 +86,7 @@ def options():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     return
+                
         pygame.display.update()
 
 def main_menu():
@@ -89,17 +94,17 @@ def main_menu():
     buttons = []
 
     PLAY_BUTTON = Button(
-        image=pygame.image.load("F:/Project/[Python] Lap trinh game/TetrisClone/graphics/Play Rect.png"), 
+        image=pygame.image.load("E:/Code/Python/Project/TetrisClone/graphics/Play Rect.png"), 
         pos=(640, 250), text_input="PLAY", font=get_font(50), 
         base_color="#d7fcd4", hovering_color="White"
     )
     OPTIONS_BUTTON = Button(
-        image=pygame.image.load("F:/Project/[Python] Lap trinh game/TetrisClone/graphics/BattleAI Rect.png"), 
+        image=pygame.image.load("E:/Code/Python/Project/TetrisClone/graphics/BattleAI Rect.png"), 
         pos=(640, 400), text_input="OPTIONS", font=get_font(50), 
         base_color="#d7fcd4", hovering_color="White"
     )
     QUIT_BUTTON = Button(
-        image=pygame.image.load("F:/Project/[Python] Lap trinh game/TetrisClone/graphics/Quit Rect.png"), 
+        image=pygame.image.load("E:/Code/Python/Project/TetrisClone/graphics/Quit Rect.png"), 
         pos=(640, 550), text_input="QUIT", font=get_font(50), 
         base_color="#d7fcd4", hovering_color="White"
     )
