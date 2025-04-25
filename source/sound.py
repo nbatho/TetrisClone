099b8210eb24156312674cf2a431799c701dce17
NamTG-PTIT <162339079+ntgitis@users.noreply.github.com>
2025-04-25 08:04:18 +0700

    update

--- source/sound.py
+++ source/sound.py
@@ -4,16 +4,17 @@ import pygame
 class SoundManager:
     def __init__(self):
         pygame.mixer.init()
-        self.winning_sound = pygame.mixer.Sound("F:/Project/[Python] Lap trinh game/TetrisClone/sounds/success.wav")
-        self.gameover_sound = pygame.mixer.Sound("F:/Project/[Python] Lap trinh game/TetrisClone/sounds/gameover.wav")
+        self.music_landing = pygame.mixer.Sound("E:/Code/Python/Project/TetrisClone/sounds/landing.wav")
+        # self.gameover_sound = pygame.mixer.Sound("F:/Project/[Python] Lap trinh game/TetrisClone/sounds/gameover.wav")
 
-    def music_win(self):
-        self.winning_sound.play()
-    def music_gameover(self):
-        self.gameover_sound.play()
+    # def music_gameover(self):
+    #     self.gameover_sound.play()
+
+    def music_landing(self):
+        self.music_landing.play()
 
     def music_theme(self):
-        pygame.mixer.music.load("F:/Project/[Python] Lap trinh game/TetrisClone/sounds/theme.mp3")
+        pygame.mixer.music.load("E:/Code/Python/Project/TetrisClone/sounds/sounds_theme.mp3")
         pygame.mixer.music.play(-1)  # Lặp vô hạn
 
     def stop_music(self):
