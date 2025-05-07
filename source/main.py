c53e44ccbfc0deedc580da7737f50203adfb1608
Pe <127192174+nbatho@users.noreply.github.com>
2025-05-07 19:59:52 +0700

    mini fix main

--- source/main.py
+++ source/main.py
@@ -10,80 +10,8 @@ from sound import *
 from menu import *
 
 
-def input_ip_screen():
-    font = pygame.font.SysFont("Arial", 36)
-    input_text = ""
-    active = True
 
-    input_rect = pygame.Rect(WINDOW_WIDTH - 150, WINDOW_HEIGHT // 2 - 25, 300, 50)
-    color_inactive = pygame.Color("gray")
-    color_active = pygame.Color("white")
-    color = color_active
 
-    while active:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    active = False
-                    return input_text.strip() if input_text.strip() else "127.0.0.1"
-                elif event.key == pygame.K_ESCAPE:
-                    return None
-                elif event.key == pygame.K_BACKSPACE:
-                    input_text = input_text[:-1]
-                else:
-                    input_text += event.unicode
-
-        screen = pygame.display.get_surface()
-        screen.fill((0, 0, 0))
-
-        # Vẽ ô nhập
-        pygame.draw.rect(screen, color, input_rect, 2)
-        text_surface = font.render(input_text, True, color)
-        screen.blit(text_surface, (input_rect.x + 10, input_rect.y + 10))
-
-        label = font.render("Enter IP host (Press ENTER)", True, "white")
-        screen.blit(label, (WINDOW_WIDTH // 2 + 115, WINDOW_HEIGHT // 2 - 80))
-
-        pygame.display.flip()
-
-def get_player_name(screen, clock, font):
-    input_box = pygame.Rect(440, 300, 400, 60)
-    color = pygame.Color('dodgerblue2')
-    text = ''
-    done = False
-
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    if text.strip() != '':
-                        return text.strip()
-                elif event.key == pygame.K_BACKSPACE:
-                    text = text[:-1]
-                else:
-                    if len(text) < 15:
-                        text += event.unicode
-
-        # Giao diện nền đen
-        screen.fill((0, 0, 0))  # màu đen
-
-        # Tiêu đề
-        title_surface = font.render("Enter your name:", True, pygame.Color('white'))
-        screen.blit(title_surface, title_surface.get_rect(center=(640, 200)))
-
-        # Khung nhập tên
-        pygame.draw.rect(screen, color, input_box, 2)
-        txt_surface = font.render(text, True, pygame.Color("white"))
-        screen.blit(txt_surface, (input_box.x + 10, input_box.y + 15))
-
-        pygame.display.flip()
-        clock.tick(30)
 
 class Main:
     def __init__(self):
@@ -133,34 +61,6 @@ class Main:
         bot_next_shape = self.opponent_next_shapes.pop(0)
         self.opponent_next_shapes.append(choice(list(TETROMINOS.keys())))
         return bot_next_shape
-    def draw_opponent_grid(self, field_data):
-        cell_size = CELL_SIZE
-        x_offset = WINDOW_WIDTH + PADDING
-        y_offset = PADDING
-        # Vẽ lưới ngang và dọc
-        for col in range(1, COLUMNS):
-            x = x_offset + col * cell_size
-            pygame.draw.line(self.display_surface, LINE_COLOR, (x, y_offset), (x, y_offset + ROWS * cell_size))
-        for row in range(1, ROWS):
-            y = y_offset + row * cell_size
-            pygame.draw.line(self.display_surface, LINE_COLOR, (x_offset, y), (x_offset + COLUMNS * cell_size, y))
-
-        # Vẽ viền quanh toàn bộ bảng
-        board_rect = pygame.Rect(x_offset, y_offset, COLUMNS * cell_size, ROWS * cell_size)
-        pygame.draw.rect(self.display_surface, LINE_COLOR, board_rect, 2, 2)
-        # Vẽ các block đã rơi
-        for y, row in enumerate(field_data):
-            for x, cell in enumerate(row):
-                if cell:
-                    rect = pygame.Rect(
-                        x_offset + x * cell_size,
-                        y_offset + y * cell_size,
-                        cell_size,
-                        cell_size
-                    )
-                    pygame.draw.rect(self.display_surface, (150, 150, 150), rect)  # màu block
-                    # pygame.draw.rect(self.display_surface, (50, 50, 50), rect, 1)  # viền ô
-
     def show_result(self, result_text,offset = 0):
         font = pygame.font.SysFont("Arial", 48)
         text_surface = font.render(result_text, True, "white")
@@ -168,6 +68,80 @@ class Main:
         self.display_surface.blit(text_surface, (PADDING + offset + 75, WINDOW_HEIGHT // 2-25))
         pygame.display.update()
         pygame.time.delay(100)
+    def get_player_name(self,screen, clock, font):
+        input_box = pygame.Rect(440, 300, 400, 60)
+        color = pygame.Color('dodgerblue2')
+        text = ''
+        done = False
+
+        while not done:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        if text.strip() != '':
+                            return text.strip()
+                    elif event.key == pygame.K_BACKSPACE:
+                        text = text[:-1]
+                    else:
+                        if len(text) < 15:
+                            text += event.unicode
+
+            # Giao diện nền đen
+            screen.fill((0, 0, 0))  # màu đen
+
+            # Tiêu đề
+            title_surface = font.render("Enter your name:", True, pygame.Color('white'))
+            screen.blit(title_surface, title_surface.get_rect(center=(640, 200)))
+
+            # Khung nhập tên
+            pygame.draw.rect(screen, color, input_box, 2)
+            txt_surface = font.render(text, True, pygame.Color("white"))
+            screen.blit(txt_surface, (input_box.x + 10, input_box.y + 15))
+
+            pygame.display.flip()
+            clock.tick(30)
+    def input_ip_screen(self):
+        font = pygame.font.SysFont("Arial", 36)
+        input_text = ""
+        active = True
+
+        input_rect = pygame.Rect(WINDOW_WIDTH - 150, WINDOW_HEIGHT // 2 - 25, 300, 50)
+        color_inactive = pygame.Color("gray")
+        color_active = pygame.Color("white")
+        color = color_active
+
+        while active:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        active = False
+                        return input_text.strip() if input_text.strip() else "127.0.0.1"
+                    elif event.key == pygame.K_ESCAPE:
+                        return None
+                    elif event.key == pygame.K_BACKSPACE:
+                        input_text = input_text[:-1]
+                    else:
+                        input_text += event.unicode
+
+            screen = pygame.display.get_surface()
+            screen.fill((0, 0, 0))
+
+            # Vẽ ô nhập
+            pygame.draw.rect(screen, color, input_rect, 2)
+            text_surface = font.render(input_text, True, color)
+            screen.blit(text_surface, (input_rect.x + 10, input_rect.y + 10))
+
+            label = font.render("Enter IP host (Press ENTER)", True, "white")
+            screen.blit(label, (WINDOW_WIDTH // 2 + 115, WINDOW_HEIGHT // 2 - 80))
+
+            pygame.display.flip()
+
     def run(self):
         game_running = True
         while game_running:
@@ -179,7 +153,7 @@ class Main:
                 self.BOT = (selection == "vsBot")
                 if self.play == 'single': 
                     offset = WINDOW_WIDTH//2 + 50
-                    self.player_name = get_player_name(self.display_surface, self.clock, pygame.font.SysFont("Arial", 36))
+                    self.player_name = self.get_player_name(self.display_surface, self.clock, pygame.font.SysFont("Arial", 36))
                 else: offset = 0
                 self.game = Game(self.get_next_shape, self.update_score, bot_enable=False, x_offset=offset)
                 self.score = Score(x_offset=offset)
@@ -190,7 +164,7 @@ class Main:
                     self.opponent_score = Score(x_offset=WINDOW_WIDTH)
                     self.opponent_preview = Preview(x_offset=WINDOW_WIDTH)
                 elif self.play == "vsPlayer":
-                    host_ip = input_ip_screen()
+                    host_ip = self.input_ip_screen()
                     if host_ip is None:
                         self.play = None
                         playing = False
@@ -206,7 +180,6 @@ class Main:
                             print(f"Connected as Player {self.player_id}")
                         except:
                             print("Could not connect to server.")
-                            # self.play = "single"
                         # Chờ đến khi cả 2 người chơi sẵn sàng
                         waiting = True
                         font = pygame.font.SysFont("Arial", 36)
